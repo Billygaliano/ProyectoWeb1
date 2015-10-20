a938a2a1ddaed3ea6fd8f9c4b9d6a87b3566aaf3
rma93 <rubenmena93@hotmail.es>
2015-10-21 01:24:36 +0200

    Actualización socios, v.1 Rubén

--- ProyectoWeb1/public_html/bin/contact_me.php
+++ ProyectoWeb1/public_html/bin/contact_me.php
@@ -3,7 +3,10 @@
 if(empty($_POST['name'])  		||
    empty($_POST['phone']) 		||
    empty($_POST['email']) 		||
-   empty($_POST['message'])	||
+   empty($_POST['message'])             ||
+   empty($_POST['apellido'])            ||
+   empty($_POST['dni'])	||
+        
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
@@ -14,11 +17,13 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
+$apellido= $_POST['apellido'];
+$dni=$_POST['dni'];
 	
 // create email body and send it	
-$to = 'your-email@your-domain.com'; // PUT YOUR EMAIL ADDRESS HERE
-$email_subject = "Modern Business Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
-$email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
+$to = 'rubenmena93@hotmail.es'; // PUT YOUR EMAIL ADDRESS HERE
+$email_subject = "Solicitud Nuevo Socio:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+$email_body = "Has recibido una nueva solicitud para socios.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nApellido: $apellido\n\nDNI: $dni\n\nTelefono: $phone\n\nEmail: $email_address\n\nMensaje:\n$message";
 $headers = "From: noreply@your-domain.com\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
